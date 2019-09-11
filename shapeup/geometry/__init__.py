a97c53c29c4bc8da44c7a84d80519f34c7008b72
patdaburu <pat@daburu.net>
2019-09-11 04:28:43 -0500

    Added geometry package.

---
+++ shapeup/geometry/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created by pat on 9/11/19
+"""
+.. currentmodule:: shapeup.geometry
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+This package contains geometry objects and utilities.
+"""
